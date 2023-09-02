1c683848fba9203b637a0b9d34a6ebf38999083e
MitsuyaT <lautyrodriguez25@gmail.com>
2023-09-02 14:39:21 -0300

    Finalizado

--- 07 - Recorta una cadena/index.js
+++ 07 - Recorta una cadena/index.js
@@ -3,3 +3,20 @@
 //Devuelve la cadena recortada con un ... al final
 
 
+function truncateString(str, num) {
+    let newArr = []; //preparo un array vacio para almacenar los nuevos datos
+    let caracters = str.split(""); //Divido por caracteres
+
+    for (let i = 0 ; i < num; i++){
+        newArr.push(caracters[i]); //Voy colocando los caracteres hasta el "NUM" indicado
+    }
+
+    newArr = newArr.join("");//Junto todos los caracteres para formar un string
+
+    if (num < str.length) {  //Si se recorta bastante el string se agrega "..." indicando que continua la frase
+        newArr = newArr + "..."
+    }
+    return newArr;
+  }
+  
+  console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
