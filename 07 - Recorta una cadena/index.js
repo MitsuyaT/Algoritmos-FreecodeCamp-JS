615fdac010a7c60bc2cd0a9888ebb110e60f0aed
MitsuyaT <lautyrodriguez25@gmail.com>
2023-09-02 13:50:15 -0300

    Comezando ejercicio 7

---
+++ 07 - Recorta una cadena/index.js
@@ -0,0 +1,5 @@
+//Recorta una cadena
+//Recorta una cadena (primer argumento) si es más larga que la longitud máxima proporcionada (segundo argumento). 
+//Devuelve la cadena recortada con un ... al final
+
+
