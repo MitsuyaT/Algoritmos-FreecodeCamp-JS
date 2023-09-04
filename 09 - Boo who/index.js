ce5b1cb19c887069b7956bd5d9f04715c9acacee
MitsuyaT <lautyrodriguez25@gmail.com>
2023-09-04 11:40:51 -0300

    Finalizado

---
+++ 09 - Boo who/index.js
@@ -0,0 +1,9 @@
+//Boo who (Booleano Quién)
+//Comprueba si el valor está clasificado como booleano primitivo. Devuelve true o false.
+//Booleanos primitivos son true y false.
+
+function booWho(bool) {
+    return (bool === true || bool === false);
+  }
+  
+  booWho(null);
